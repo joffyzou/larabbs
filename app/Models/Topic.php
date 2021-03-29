55b662332a2923f72b8a4107515a02745edabac2
Joffy <326535465@qq.com>
2021-03-29 23:56:41 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -20,4 +20,14 @@ class Topic extends Model
         'excerpt', 
         'slug'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
