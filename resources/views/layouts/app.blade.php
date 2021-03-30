46179d99b8267e9bb406fef177ea918290e5837c
Joffy <326535465@qq.com>
2021-03-30 19:16:21 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'LaraBBS') - Laravel 进阶课程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @yield('styles')
 </head>
 
 <body>
@@ -19,5 +20,6 @@
     @include('layouts._footer')
 </div>
 <script src="{{ mix('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
