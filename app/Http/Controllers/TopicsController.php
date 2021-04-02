b4be8585ae0270c87136a82b0ed81847fc3682bc
Joffy <326535465@qq.com>
2021-04-02 13:03:03 +0800

    翻译队列化

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -48,7 +48,7 @@ class TopicsController extends Controller
 		$topic->user_id = Auth::id();
 		$topic->save();
 
-		return redirect()->$topic->link()->with('success', '帖子创建成功！');
+		return redirect()->to($topic->link())->with('success', '帖子创建成功！');
 	}
 
 	public function edit(Topic $topic)
@@ -64,7 +64,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->$topic->link()->with('success', '更新成功！');
+		return redirect()->to($topic->link())->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
